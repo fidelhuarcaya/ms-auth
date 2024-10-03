3e29341b752a826959150ab5eebfa12a916a4d6f
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2024-10-03 11:57:39 -0500

    feat: add tomcat server initializer

--- src/main/java/org/copper/auth/MsAuthApplication.java
+++ src/main/java/org/copper/auth/MsAuthApplication.java
@@ -2,9 +2,10 @@ package org.copper.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MsAuthApplication {
+public class MsAuthApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MsAuthApplication.class, args);
