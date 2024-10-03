e85b2b49d74a57735fbf0965aa1745f7c14509f9
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2024-10-03 17:20:47 -0500

    feat: add port 8080 to Dockerfile

--- src/main/java/org/copper/auth/MsAuthApplication.java
+++ src/main/java/org/copper/auth/MsAuthApplication.java
@@ -2,6 +2,7 @@ package org.copper.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -10,5 +11,8 @@ public class MsAuthApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(MsAuthApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MsAuthApplication.class);
+    }
 }
