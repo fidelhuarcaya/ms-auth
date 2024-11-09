c17648ff723e1ebfeadfb61a5429ea02a994d208
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2024-11-08 20:33:46 -0500

    feat: add id to Claims in the JWT

--- src/main/java/org/copper/auth/entity/Auditable.java
+++ src/main/java/org/copper/auth/entity/Auditable.java
@@ -1,24 +1,36 @@
 package org.copper.auth.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class Auditable {
+  @Column(name = "created_at", updatable = false)
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at")
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
+  @CreatedBy
+  @Column(name = "created_by", updatable = false)
   private String createdBy;
 
-  @CreationTimestamp
-  private LocalDateTime createdDate;
-
+  @LastModifiedBy
+  @Column(name = "updated_by")
   private String updatedBy;
-
-  @UpdateTimestamp
-  private LocalDateTime updatedDate;
 }
